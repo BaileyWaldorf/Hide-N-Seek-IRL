4d3b0d554047573f0664e86faaf67ab7cf9a768c
Bailey Waldorf <33463021+BaileyWaldorf@users.noreply.github.com>
2018-09-03 14:17:31 -0400

    added UI for login page

--- src/components/screens/accounts.js
+++ src/components/screens/accounts.js
@@ -1,36 +1,95 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Alert, Button, StyleSheet, StatusBar } from 'react-native';
 
-
-
+let hello = "Not a member? Sign up!"
 
 //creating a component
-class LoginForm extends Component {
+class LoginForm extends React.Component {
+     constructor(props) {
+          super(props);
+          this.state = {
+               email: "",
+               password: "",
+               member: true
+          };
+     }
+
+     handleEmailInput = (text) => {
+       this.setState({ email: text })
+     }
+
+     handlePasswordInput = (text) => {
+       this.setState({ password: text })
+     }
+
+     loginPage = () => {
+          return (
+               <View>
+                    <TextInput style = {styles.input}
+                         autoCapitalize='none'
+                         autoCorrect={false}
+                         keyboardType='email-address'
+                         returnKeyType='next'
+                         placeholder='johnsmith@email.com'
+                         placeholderTextColor='grey'
+                         onChangeText = {this.handleEmailInput}/>
+
+                    <TextInput style = {styles.input}
+                         returnKeyType='go'
+                         placeholder='password'
+                         placeholderTextColor='grey'
+                         secureTextEntry
+                         onChangeText = {this.handlePasswordInput}/>
+                    <TouchableOpacity
+                         style= {styles.buttonContainer}
+                         onPress={console.log("logged in!!")}>
+                              <Text style={styles.buttonText}>LOGIN</Text>
+                    </TouchableOpacity>
+                    <Button
+                         style={{margin: 10}}
+                         onPress={() => this.setState({member: false})}
+                         title={hello}
+                         color="steelblue"
+                    />
+               </View>
+          )
+     }
+
+     signUpPage = () => {
+          return (
+               <View>
+                    <TextInput style = {styles.input}
+                         autoCapitalize='none'
+                         autoCorrect={false}
+                         keyboardType='email-address'
+                         returnKeyType='next'
+                         placeholder='johnsmith@email.com'
+                         placeholderTextColor='grey'
+                         onChangeText = {this.handleEmailInput}/>
+
+                    <TextInput style = {styles.input}
+                         returnKeyType='go'
+                         placeholder='password'
+                         placeholderTextColor='grey'
+                         secureTextEntry
+                         onChangeText = {this.handlePasswordInput}/>
+                    <TouchableOpacity
+                         style= {styles.buttonContainer}
+                         onPress={console.log("signed up!")}>
+                              <Text style={styles.buttonText}>SIGN UP</Text>
+                    </TouchableOpacity>
+               </View>
+          )
+     }
+
     render() {
         return (
-            <view style={styles.container}>
-                <TextInput style = {styles.input}
-                    autoCapitalize='none'
-                    onSubmitEditing={() => this.passwordInput.focus()}
-                    autoCorrect={false}
-                    keyboardType='email-address'
-                    returnKeyType='next'
-                    placeholder='johnsmith@email.com'
-                    placeholderTextColor='white'/>
-                
-                <TextInput style = {styles.input}
-                    returnKeyType='go'
-                    ref={(input)=> this.passwordInput = input}
-                    placeholder='password'
-                    placeholderTextColor='rgba(225,225,225,0.7)'
-                    secureTextEntry/>
-                
-                <TouchableOpacity style= {styles.buttonContainer}
-                    onPress={onButtonPress}>
-                    <Text style={styles.buttonText}>LOGIN</Text>
-        </TouchableOpacity>
-            
-            </view>
+            <View style={styles.container}>
+                 {this.state.member
+                      ? this.loginPage()
+                      : this.signUpPage()
+                 }
+          </View>
         );
     }
 }
@@ -51,28 +110,40 @@ class Login extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backfaceVisibility: 'visible',
         width: '100%',
-        justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#FFFFFF',
+        justifyContent: 'center'
     },
 
     input: {
         height: 40,
-        backgroundColor: '#FFBBAA',
-        marginBottom: 10,
+        width: '60%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FFF',
+        borderWidth: 0,
+        borderBottomWidth: 1,
+        borderColor: 'tomato',
+        margin: 10,
         padding: 10,
-        color: 'red'
     },
 
     buttonContainer: {
-        backgroundColor: 'black',
-        paddingVertical: 15
+         height: 44,
+         width: 100,
+         marginTop: 30,
+         backgroundColor: 'tomato',
+         borderRadius: 4,
+         alignItems: 'center',
+         justifyContent: 'center',
+         borderBottomWidth: 4,
+         borderColor: '#d64c33'
     },
     buttonText: {
-        color: '#fff', 
+        color: '#fff',
         textAlign: 'center',
-        fontWeight: '700'
+        fontWeight: 'bold'
     }
 });
 
