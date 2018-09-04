ede936b1fbce2738774845de25c58c7caa709ebf
Bailey Waldorf <33463021+BaileyWaldorf@users.noreply.github.com>
2018-09-04 00:57:38 -0400

    initialized USPS API

--- src/components/screens/accounts.js
+++ src/components/screens/accounts.js
@@ -5,59 +5,59 @@ let hello = "Not a member? Sign up!"
 
 //creating a component
 class LoginForm extends React.Component {
-     constructor(props) {
-          super(props);
-          this.state = {
-               email: "",
-               password: "",
-               member: true
-          };
-     }
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: "",
+			password: "",
+			member: true
+		};
+	}
 
-     handleEmailInput = (text) => {
-       this.setState({ email: text })
-     }
+	handleEmailInput = (text) => {
+		this.setState({ email: text })
+	}
 
-     handlePasswordInput = (text) => {
-       this.setState({ password: text })
-     }
+	handlePasswordInput = (text) => {
+		this.setState({ password: text })
+	}
 
-     loginPage = () => {
-          return (
-               <View>
-                    <TextInput style = {styles.input}
-                         autoCapitalize='none'
-                         autoCorrect={false}
-                         keyboardType='email-address'
-                         returnKeyType='next'
-                         placeholder='johnsmith@email.com'
-                         placeholderTextColor='grey'
-                         onChangeText = {this.handleEmailInput}/>
+	loginPage = () => {
+		return (
+			<View style={styles.container}>
+				<TextInput style = {styles.input}
+					autoCapitalize='none'
+					autoCorrect={false}
+					keyboardType='email-address'
+					returnKeyType='next'
+					placeholder='johnsmith@email.com'
+					placeholderTextColor='grey'
+					onChangeText = {this.handleEmailInput}/>
 
-                    <TextInput style = {styles.input}
-                         returnKeyType='go'
-                         placeholder='password'
-                         placeholderTextColor='grey'
-                         secureTextEntry
-                         onChangeText = {this.handlePasswordInput}/>
-                    <TouchableOpacity
-                         style= {styles.buttonContainer}
-                         onPress={console.log("logged in!!")}>
-                              <Text style={styles.buttonText}>LOGIN</Text>
-                    </TouchableOpacity>
-                    <Button
-                         style={{margin: 10}}
-                         onPress={() => this.setState({member: false})}
-                         title={hello}
-                         color="steelblue"
-                    />
-               </View>
-          )
-     }
+				<TextInput style = {styles.input}
+					returnKeyType='go'
+					placeholder='password'
+					placeholderTextColor='grey'
+					secureTextEntry
+					onChangeText = {this.handlePasswordInput}/>
+				<TouchableOpacity
+					style= {styles.buttonContainer}
+					onPress={console.log("logged in!!")}>
+						<Text style={styles.buttonText}>LOGIN</Text>
+				</TouchableOpacity>
+				<Button
+					style={{margin: 10}}
+					onPress={() => this.setState({member: false})}
+					title={hello}
+					color="steelblue"
+				/>
+			</View>
+		)
+	}
 
      signUpPage = () => {
           return (
-               <View>
+               <View style={styles.container}>
                     <TextInput style = {styles.input}
                          autoCapitalize='none'
                          autoCorrect={false}
@@ -94,19 +94,6 @@ class LoginForm extends React.Component {
     }
 }
 
-
-
-class Login extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Lin</Text>
-            </View>
-        );
-    }
-}
-
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -115,7 +102,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-
     input: {
         height: 40,
         width: '60%',
@@ -128,7 +114,6 @@ const styles = StyleSheet.create({
         margin: 10,
         padding: 10,
     },
-
     buttonContainer: {
          height: 44,
          width: 100,
--- src/components/screens/add-package.js
+++ src/components/screens/add-package.js
@@ -2,149 +2,167 @@ import React from "react";
 import { Text, Image, TextInput, TouchableOpacity, View, StyleSheet, Button } from "react-native";
 
 export default class Add extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showTutorial: false,
-      trackingNumber: "",
-      carrier: "",
-      itemName: "",
-      sender: ""
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			showTutorial: false,
+			trackingNumber: "",
+			carrier: "",
+			itemName: "",
+			sender: "",
+			packageData: []
+		};
+		this.addPackage = this.addPackage.bind(this)
+	}
 
-  handleTrackingNumberInput = (number) => {
-    this.setState({ trackingNumber: number })
-  }
+	addPackage = (carrier) => {
+		const parseString = require('react-native-xml2js').parseString;
 
-  handleCarrierInput = (text) => {
-    this.setState({ carrier: text })
-  }
+		fetch('http://production.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML=%3C?xml%20version=%221.0%22%20encoding=%22UTF-8%22%20?%3E%20%3CTrackRequest%20USERID=%22901UNIVE3130%22%3E%20%3CTrackID%20ID=%229400111298370362821472%22%3E%3C/TrackID%3E%3C/TrackRequest%3E')
+			.then(response => response.text())
+			.then((response) => {
+				parseString(response, (err, result) => {
+					this.setState({packageData: result}, () => {
+						console.log(this.state.packageData)
+					});
+				});
+			}).catch((err) => {
+				console.log('fetch', err)
+			})
+	}
 
-  handleItemNameInput = (text) => {
-    this.setState({ itemName: text })
-  }
+	handleTrackingNumberInput = (number) => {
+		this.setState({ trackingNumber: number })
+	}
 
-  handleSenderInput = (text) => {
-    this.setState({ sender: text })
-  }
+	handleCarrierInput = (text) => {
+		this.setState({ carrier: text })
+	}
 
-  tutorialScreen = () => {
-    return(
-      <View>
-        <Text>
-          This is where you can add your tracking number.
-          Simply type in the number or scan a barcode!
-        </Text>
-        <Button
-          style={styles.button}
-          onPress={() => this.setState({showTutorial: false})}
-          title="Get Started"
-        />
-      </View>
-    )
-  }
+	handleItemNameInput = (text) => {
+		this.setState({ itemName: text })
+	}
 
-  addScreen = () => {
-    return(
-      <View>
-        <View style={{flexDirection:'row', flexWrap:'wrap', alignItems: 'center', justifyContent: 'center'}}>
-          <TextInput style = {styles.trackingNumberInput}
-            underlineColorAndroid = "transparent"
-            placeholder = "Tracking Number"
-            placeholderTextColor = "grey"
-            autoCapitalize = "none"
-            onChangeText = {this.handleTrackingNumberInput}
-          />
-          <TouchableOpacity style={styles.barcodeButton} onPress={() => this.setState({showTutorial: true})}>
-            <Image style={{width: 32, height: 32}} source={require("../assets/barcode-icon.png")}/>
-          </TouchableOpacity>
-        </View>
-        <TextInput style = {styles.input}
-          underlineColorAndroid = "transparent"
-          placeholder = "Carrier"
-          placeholderTextColor = "grey"
-          autoCapitalize = "none"
-          onChangeText = {this.handleCarrierInput}
-        />
-        <TextInput style = {styles.input}
-          underlineColorAndroid = "transparent"
-          placeholder = "Item Name (optional)"
-          placeholderTextColor = "grey"
-          autoCapitalize = "none"
-          onChangeText = {this.handleItemNameInput}
-        />
-        <TextInput style = {styles.input}
-          underlineColorAndroid = "transparent"
-          placeholder = "Sender (optional)"
-          placeholderTextColor = "grey"
-          autoCapitalize = "none"
-          onChangeText = {this.handleSenderInput}
-        />
-        <View style={{alignItems: 'center'}}>
-          <TouchableOpacity style={styles.addButton} onPress={() => this.setState({showTutorial: true})}>
-            <Text style={{color: 'white', fontWeight: 'bold'}}>ADD</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    )
-  }
+	handleSenderInput = (text) => {
+		this.setState({ sender: text })
+	}
 
-  render() {
-    return (
-      <View style={styles.container}>
-        {this.state.showTutorial
-          ? this.tutorialScreen()
-          : this.addScreen()
-        }
-      </View>
-    );
-  }
+	tutorialScreen = () => {
+		return(
+			<View>
+				<Text>
+					This is where you can add your tracking number.
+					Simply type in the number or scan a barcode!
+				</Text>
+				<Button
+					style={styles.button}
+					onPress={() => this.setState({showTutorial: false})}
+					title="Get Started"
+				/>
+			</View>
+		)
+	}
+
+	addScreen = () => {
+		return(
+			<View>
+				<View style={{flexDirection:'row', flexWrap:'wrap', alignItems: 'center', justifyContent: 'center'}}>
+					<TextInput style = {styles.trackingNumberInput}
+						underlineColorAndroid = "transparent"
+						placeholder = "Tracking Number"
+						placeholderTextColor = "grey"
+						autoCapitalize = "none"
+						onChangeText = {this.handleTrackingNumberInput}
+					/>
+					<TouchableOpacity style={styles.barcodeButton} onPress={() => this.setState({showTutorial: true})}>
+						<Image style={{width: 32, height: 32}} source={require("../assets/barcode-icon.png")}/>
+					</TouchableOpacity>
+				</View>
+				<TextInput style = {styles.input}
+				underlineColorAndroid = "transparent"
+				placeholder = "Carrier"
+				placeholderTextColor = "grey"
+				autoCapitalize = "none"
+				onChangeText = {this.handleCarrierInput}
+				/>
+				<TextInput style = {styles.input}
+				underlineColorAndroid = "transparent"
+				placeholder = "Item Name (optional)"
+				placeholderTextColor = "grey"
+				autoCapitalize = "none"
+				onChangeText = {this.handleItemNameInput}
+				/>
+				<TextInput style = {styles.input}
+				underlineColorAndroid = "transparent"
+				placeholder = "Sender (optional)"
+				placeholderTextColor = "grey"
+				autoCapitalize = "none"
+				onChangeText = {this.handleSenderInput}
+				/>
+				<View style={{alignItems: 'center'}}>
+					<TouchableOpacity style={styles.addButton} onPress={this.addPackage}>
+						<Text style={{color: 'white', fontWeight: 'bold'}}>ADD</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
+		)
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				{this.state.showTutorial
+					? this.tutorialScreen()
+					: this.addScreen()
+				}
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  input: {
-    margin: 10,
-    height: 40,
-    width: 250,
-    borderColor: 'tomato',
-    borderWidth: 0,
-    borderBottomWidth: 1
-  },
-  trackingNumberInput: {
-    margin: 10,
-    height: 40,
-    width: 250,
-    borderColor: 'tomato',
-    borderWidth: 0,
-    borderBottomWidth: 1,
-    alignItems: 'flex-start'
-  },
-  barcodeButton: {
-    height: 35,
-    width: 45,
-    right: 10,
-    backgroundColor: 'white',
-    borderRadius: 4,
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute'
-  },
-  addButton: {
-    height: 44,
-    width: 100,
-    marginTop: 30,
-    backgroundColor: 'tomato',
-    borderRadius: 4,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderBottomWidth: 4,
-    borderColor: '#d64c33'
-  },
-  container: {
-    flex: 1,
-    padding: 50,
-    alignItems: "center",
-    justifyContent: "center"
-  }
+	input: {
+		margin: 10,
+		height: 40,
+		width: 250,
+		borderColor: 'tomato',
+		borderWidth: 0,
+		borderBottomWidth: 1
+	},
+	trackingNumberInput: {
+		margin: 10,
+		height: 40,
+		width: 250,
+		borderColor: 'tomato',
+		borderWidth: 0,
+		borderBottomWidth: 1,
+		alignItems: 'flex-start'
+	},
+	barcodeButton: {
+		height: 35,
+		width: 45,
+		right: 10,
+		backgroundColor: 'white',
+		borderRadius: 4,
+		alignItems: 'center',
+		justifyContent: 'center',
+		position: 'absolute'
+	},
+	addButton: {
+		height: 44,
+		width: 100,
+		marginTop: 30,
+		backgroundColor: 'tomato',
+		borderRadius: 4,
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderBottomWidth: 4,
+		borderColor: '#d64c33'
+	},
+	container: {
+		flex: 1,
+		padding: 50,
+		alignItems: "center",
+		justifyContent: "center"
+	}
 });
